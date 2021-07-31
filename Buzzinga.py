cba3d323ae5c858e1f88664730533b266d3cdfdd
Steffen Schroedter <List@MacBook-Air.fritz.box>
2021-07-31 22:14:41 +0200

    chmod deleted

--- Buzzinga.py
+++ Buzzinga.py
@@ -99,7 +99,7 @@ def delete_category(game_dir):
         os.chdir(game_dir)
         for f in os.listdir(game_dir):
                 if not os.path.isdir(game_dir+"/"+f):
-                        os.chmod(game_dir+"/"+f, 0o777)
+                        #os.chmod(game_dir+"/"+f, 0o777)
                         os.remove(game_dir+"/"+f)
                 else:
                         pass
