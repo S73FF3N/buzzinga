6e984c726badad1ee914b17ceb7dd3cca90fcceb
Steffen Schroedter <List@MacBook-Air.fritz.box>
2021-10-20 19:43:31 +0200

    create required folders on buzzinga start

--- Buzzinga.py
+++ Buzzinga.py
@@ -288,7 +288,7 @@ def choose_category(import_status=""):
                 page_nr = 1
                 for item in os.listdir(game_folder):
                         x, y, w, h = button_layout_28[int(game_nr - 1)]
-                        if item.lower().endswith('.json') and item.lower().startswith('.'):
+                        if item.lower().endswith('.json') and not item.lower().startswith('.'):
                                 buttons['page ' + str(page_nr)].append([os.path.splitext(item)[0], x, y, w, h])
                         else:
                                 buttons['page ' + str(page_nr)].append([item, x, y, w, h])
