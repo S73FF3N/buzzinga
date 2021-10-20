be66de812f4c9f986f23b697e1f9870f23b5b02b
Steffen Schroedter <List@MacBook-Air.fritz.box>
2021-10-20 21:10:20 +0200

    create required folders on buzzinga start

--- check_usb_input.py
+++ check_usb_input.py
@@ -33,7 +33,8 @@ def usb_input_check(done, images_imported, time_consumed):
 							os.mkdir(dir_name.decode('utf-8'))
 						for f in os.listdir(item + b'/Bilder/' + category):
 							file = item + b'/Bilder/' + category + b'/' + f
-							file_renamed = item + b'/Bilder/' + category + b'/' + f.replace(" ", "_")
+							f_renamed = f.replace(" ", "_")
+							file_renamed = item + b'/Bilder/' + category + b'/' + f_renamed
 							os.popen("mv {} {}".format(file.decode('utf-8'), file_renamed.decode('utf-8')))
 						for f in os.listdir(item + b'/Bilder/' + category):
 							if not os.path.isfile(b'/home/pi/Desktop/SdR/Bilder/'+category+b'/'+f) and f.lower().endswith((b'.png', b'.jpg', b'.jpeg', b'.bmp')):
