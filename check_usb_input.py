3a8d738f7247351ae61523ee6a3af56631b224a6
Steffen Schroedter <List@MacBook-Air.fritz.box>
2021-10-20 20:50:03 +0200

    create required folders on buzzinga start

--- check_usb_input.py
+++ check_usb_input.py
@@ -32,7 +32,7 @@ def usb_input_check(done, images_imported, time_consumed):
 							dir_name = b'/home/pi/Desktop/SdR/Bilder/'+category
 							os.mkdir(dir_name.decode('utf-8'))
 						for f in os.listdir(item + b'/Bilder/' + category):
-							os.rename(item+b'/Bilder/'+category+b'/'+f, item+b'/Bilder/'+category+b'/'+f.replace(" ", "_"))
+							os.rename(item+b'/Bilder/'+category+b'/'+f, os.path.join(item+b'/Bilder/'+category, f.replace(" ", "_")))
 						for f in os.listdir(item + b'/Bilder/' + category):
 							if not os.path.isfile(b'/home/pi/Desktop/SdR/Bilder/'+category+b'/'+f) and f.lower().endswith((b'.png', b'.jpg', b'.jpeg', b'.bmp')):
 								file_to_copy = item+b'/Bilder/'+category+b'/'+f
