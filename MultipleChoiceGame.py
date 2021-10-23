c82cc4e318d63c1d9004695de803953a1e62f879
Steffen Schroedter <List@MacBook-Air.fritz.box>
2021-10-23 11:38:27 +0200

    show which player has already answered

--- MultipleChoiceGame.py
+++ MultipleChoiceGame.py
@@ -110,6 +110,16 @@ def multiple_choice_game(players, playerNamesList, content_dir, screen, screenx,
     game_sound_channel = pygame.mixer.Channel(1)
 
     def random_pick_content():
+        global winner_found
+        global player1_locked
+        global player2_locked
+        global player3_locked
+        global player4_locked
+        global random_key
+        global random_val
+        global winner_found
+        global solution_dict
+        global player_answers
         try:
             random_key = random.choice(list(content_dict.keys()))
             random_val = content_dict[random_key]
