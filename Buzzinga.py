5978d8153ff4f0ad2d6ed2e7bc1d61a7037e03ca
Steffen Schroedter <List@MacBook-Air.fritz.box>
2021-10-24 21:22:44 +0200

    added update method on start screen

--- Buzzinga.py
+++ Buzzinga.py
@@ -152,12 +152,12 @@ def start_screen_setup(update_status=""):
         update_status = update_status.decode("utf-8")
         update_status = update_status.split("\n")
         print(update_status, type(update_status))
-        #line = 0
-        #for l in update_status:
-        #        text_surf, text_rect = text_objects(l, MINI_TEXT)
-        #        text_rect.center = (int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 10*(8+line)))
-        #        SCREEN.blit(text_surf, text_rect)
-        #        line += 0.4
+        line = 0
+        for l in update_status:
+                text_surf, text_rect = text_objects(l, MINI_TEXT)
+                text_rect.center = (int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 10*(8+line)))
+                SCREEN.blit(text_surf, text_rect)
+                line += 0.4
         pygame.display.update()
 
 def start_screen(update_status=""):
