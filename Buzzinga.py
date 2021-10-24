1892235a1a59b62f199301e34eb9be4125b0a45c
Steffen Schroedter <List@MacBook-Air.fritz.box>
2021-10-24 21:28:59 +0200

    added update method on start screen

--- Buzzinga.py
+++ Buzzinga.py
@@ -149,9 +149,9 @@ def start_screen_setup(update_status=""):
         text_rect.center = (int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 10*7))
         SCREEN.blit(text_surf, text_rect)
         print(update_status, type(update_status))
-        update_status = b'update_status'
-        print(update_status, type(update_status))
+        #update_status = b'update_status'
         update_status = update_status.decode("utf-8")
+        print(update_status, type(update_status))
         update_status = update_status.split("\n")
         print(update_status, type(update_status))
         line = 0
