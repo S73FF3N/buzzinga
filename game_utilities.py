4b24a0a0c5480b0032324c2773231af9f1ddb244
Steffen Schroedter <List@MacBook-Air.fritz.box>
2021-11-09 20:57:14 +0100

    check usb input

--- game_utilities.py
+++ game_utilities.py
@@ -53,7 +53,7 @@ def mp3_to_wav(mp3_file):
     if mp3_file[-3:] == "wav":
         file_out = mp3_file
     else:
-        conversion = subprocess.Popen('ffmpeg -i ' + mp3_file + ' ' + mp3_file[:-3] + 'wav', shell=True)
+        conversion = subprocess.Popen('sudo ffmpeg -i ' + mp3_file + ' ' + mp3_file[:-3] + 'wav', shell=True)
         subprocess.Popen.wait(conversion)
         os.remove(mp3_file)
         file_out = mp3_file[:-3] + 'wav'
