1932cc5c7be0f3bacf9f1b9740b926b748b57065
Steffen Schroedter <List@MacBook-Air.fritz.box>
2021-11-13 21:32:30 +0100

    added sudo to apt-get

--- update_buzzinga.py
+++ update_buzzinga.py
@@ -9,17 +9,9 @@ time_consumed = 0
 
 def run_update_buzzinga():
     try:
-        #os.system("sudo add-apt-repository universe")
         os.system("sudo apt-get -qq update")
         os.system("sudo apt-get -qq upgrade")
         os.system("sudo apt-get --yes --assume-yes install lame")
-        #os.system("sudo apt -qq autoremove")
-        #os.system("sudo apt-get -qq install libwebpmux3")
-        #os.system("sudo apt-get -qq install libavdevice58")
-        #os.system("sudo apt-get -qq install libavfilter7")
-        #os.system("sudo apt-get -qq install libavformat58")
-        #os.system("sudo apt-get -qq install libavcodec58")
-        #os.system("sudo apt-get -qq --yes --assume-yes install ffmpeg")
         os.chdir("/home/pi/Desktop/venv/mycode/")
         os.system("git pull origin master")
         return
