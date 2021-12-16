d202897c9f9079c427621044f10c9202d071fa18
S73FF3N <stschroedter@yahoo.de>
2021-12-16 20:41:34 +0100

    first show players answers, then solution

--- MultipleChoiceGame.py
+++ MultipleChoiceGame.py
@@ -344,27 +344,27 @@ def multiple_choice_game(players, playerNamesList, content_dir, screen, screenx,
                             os.chdir("/home/pi/Desktop/venv/mycode/")
                             break_flag = True
                             running = False
-                    if buttonpressed == pygame.K_RETURN:
-                        pygame.draw.rect(screen, Static.WHITE, solution_container)
-                        pygame.display.flip()
-                        # reset the buzzers to black
-                        for n in range(0, players):
-                            player_buzzer_container = pygame.Rect(picture_container_width, (
-                                    game_label_container_height + player_label_container_height + n * player_container_height),
-                                                                  player_buzzer_container_width,
-                                                                  player_buzzer_container_height)
-                            pygame.draw.rect(screen, Static.BLACK, player_buzzer_container)
-                        pygame.draw.rect(screen, Static.WHITE, picture_counter_container)
-                        nr += 1
-                        progress = myfont.render(str(nr) + "/" + str(amount_of_content), 1, Static.RED)
-                        pygame.display.flip()
-                        random_pick_content()
-                        pygame.display.flip()
-                        question_answered = False
-                        solution_shown = "Waiting"
-                        pygame.display.flip()
+                        if buttonpressed == pygame.K_RETURN:
+                            pygame.draw.rect(screen, Static.WHITE, solution_container)
+                            pygame.display.flip()
+                            # reset the buzzers to black
+                            for n in range(0, players):
+                                player_buzzer_container = pygame.Rect(picture_container_width, (
+                                        game_label_container_height + player_label_container_height + n * player_container_height),
+                                                                      player_buzzer_container_width,
+                                                                      player_buzzer_container_height)
+                                pygame.draw.rect(screen, Static.BLACK, player_buzzer_container)
+                            pygame.draw.rect(screen, Static.WHITE, picture_counter_container)
+                            nr += 1
+                            progress = myfont.render(str(nr) + "/" + str(amount_of_content), 1, Static.RED)
+                            pygame.display.flip()
+                            random_pick_content()
+                            pygame.display.flip()
+                            question_answered = False
+                            solution_shown = "Waiting"
+                            pygame.display.flip()
 
-            # solution has been displayed
+            # answers have been shown
             while solution_shown == "Prepared" and not break_flag:
                 for event in pygame.event.get():
                     if event.type == pygame.KEYDOWN:
