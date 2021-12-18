348093880f830f29ba1b6a5410b103017ab940a8
S73FF3N <stschroedter@yahoo.de>
2021-12-18 19:21:40 +0100

    gitignore png and bmp files

--- update_buzzinga.py
+++ update_buzzinga.py
@@ -9,12 +9,13 @@ time_consumed = 0
 
 def run_update_buzzinga():
     try:
-        os.system("sudo apt-get -qq update")
-        os.system("sudo apt-get -qq upgrade")
-        os.system("sudo apt-get -qq --yes --assume-yes install lame")
-        os.chdir("/home/pi/Desktop/venv/mycode/")
+        #os.system("sudo apt-get -qq update")
+        #os.system("sudo apt-get -qq upgrade")
+        #os.system("sudo apt-get -qq --yes --assume-yes install lame")
+        #os.chdir("/home/pi/Desktop/venv/mycode/")
         os.system("git remote prune origin")
         os.system("git pull origin master")
+        #os.system("sudo git reset --hard origin/master")
         return
     except:
         return "Update nicht erfolgreich."
