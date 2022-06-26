d9bdfb4025037e6427bae5f01af1f8b541875cb9
S73FF3N <stschroedter@yahoo.de>
2022-06-26 19:32:08 +0200

    avoid repeating of countdown

--- WhoKnowsMoreGame.py
+++ WhoKnowsMoreGame.py
@@ -245,6 +245,7 @@ def who_knows_more_game(players, playerNamesList, content_dir, screen, screenx, 
 
         while correct_answer:
             global random_val
+            countdown_ended = False
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
